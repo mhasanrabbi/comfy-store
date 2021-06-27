b0510458fcfb1777e211dfb8e43cfa5a39d2ef91
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-06-27 13:52:18 +0600

    react router setup

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Home from './HomePage';
+import Products from './ProductsPage';
+import SingleProduct from './SingleProductPage';
+import About from './AboutPage';
+import Cart from './CartPage';
+import CheckOut from './CheckoutPage';
+import Error from './ErrorPage';
+import PrivateRoute from './PrivateRoute';
+
+export 
+{ 
+  Home, 
+  Products, 
+  SingleProduct, 
+  About,
+  Cart, 
+  CheckOut,
+  Error, 
+  PrivateRoute
+} 

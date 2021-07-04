689916bc1a244451309fa1931e89429c71d5a710
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-07-04 22:18:22 +0600

    home page -hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,26 @@ import heroBcg from '../assets/hero-bcg.jpeg'
 import heroBcg2 from '../assets/hero-bcg-2.jpeg'
 
 const Hero = () => {
-  return <h4>hero</h4>
+  return (
+    <Wrapper className="section-center">
+      <article className="content">
+        <h1>
+          design your <br/>
+          comfort zone
+        </h1>
+        <p>
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident
+        </p>
+        <Link to="/products" className='btn hero-btn'>
+          shop now
+        </Link>
+      </article>
+      <article className="img-container">
+        <img src={heroBcg} alt="nice table" className="main-img" />
+        <img src={heroBcg2} alt="person working" className="accent-img" />
+      </article>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`

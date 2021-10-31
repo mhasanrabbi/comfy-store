d67968ab457625e27905dd3c309e0649ef5975be
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-10-31 13:18:38 +0600

    filters default values

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -11,10 +11,14 @@ import {
 
 const filter_reducer = (state, action) => {
   if (action.type === LOAD_PRODUCTS) {
+    let maxPrice = action.payload.map((p) => p.price);
+    maxPrice = Math.max(...maxPrice);
+
     return {
       ...state,
       all_products: [...action.payload],
       filtered_products: [...action.payload],
+      filters: { ...state.filters, max_price: maxPrice, price: maxPrice },
     };
   }
   if (action.type === SET_GRIDVIEW) {
@@ -53,6 +57,7 @@ const filter_reducer = (state, action) => {
         return b.name.localeCompare(a.name);
       });
     }
+
     return { ...state, filtered_products: tempProducts };
   }
   throw new Error(`No Matching "${action.type}" - action type`);

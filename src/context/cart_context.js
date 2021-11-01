2ff5593630acd543b50ac51ad00b5fab609c6549
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-11-01 22:31:54 +0600

    cart item

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -18,7 +18,7 @@ const getLocalStorage = () => {
 };
 
 const initialState = {
-  cart: getLocalStorage,
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
   shipping_fee: 534,
@@ -28,23 +28,36 @@ const CartContext = React.createContext();
 
 export const CartProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
-
   // add to cart
-  const addTocart = (id, color, amount, product) => {
+  const addToCart = (id, color, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } });
   };
   // remove item
-  const removeItem = (id) => {};
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_CART_ITEM, payload: id });
+  };
   // toggle amount
-  const toggleAmount = (id, value) => {};
+  const toggleAmount = (id, value) => {
+    dispatch({
+      type: TOGGLE_CART_ITEM_AMOUNT,
+      payload: {
+        id,
+        value,
+      },
+    });
+  };
   // clear cart
-  const clearCart = () => {};
-
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  };
   useEffect(() => {
     localStorage.setItem('cart', JSON.stringify(state.cart));
+    dispatch({ type: COUNT_CART_TOTALS });
   }, [state.cart]);
   return (
-    <CartContext.Provider value={{ ...state, addTocart }}>
+    <CartContext.Provider
+      value={{ ...state, addToCart, removeItem, toggleAmount, clearCart }}
+    >
       {children}
     </CartContext.Provider>
   );
